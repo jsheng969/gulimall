b75f8f4896c8f2f69399a98eb41d87b3f6badec6
jsheng969 <jsheng969@outlook.com>
2021-07-08 17:47:45 +0800

    nacos注册中心、配置中心

--- gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
+++ gulimall-member/src/main/java/com/atguigu/gulimall/member/GulimallMemberApplication.java
@@ -3,9 +3,13 @@ package com.atguigu.gulimall.member;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients(basePackages = "com.atguigu.gulimall.member.feign")
 @SpringBootApplication
 @MapperScan("com.atguigu.gulimall.member.dao")
+@EnableDiscoveryClient
 public class GulimallMemberApplication {
 
     public static void main(String[] args) {
